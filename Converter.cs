4eda7e537bc2471d872c07879703f30a2f9134cd
jtschwantes <jtschwantes@gmail.com>
2019-06-07 11:14:49 -0600

    Wipe, starting fresh

---
+++ Converter.cs
@@ -0,0 +1,13 @@
+using Newtonsoft.Json.Linq;
+
+namespace Wololo
+{
+    class Converter
+    {
+        public JArray jarray{get;set;}
+
+        public static void CsvIn() {
+
+        }
+    }
+}
