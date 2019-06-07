004ce68a8e4a162029987941006e83365d795ca7
Matt Wyndham <matthewwyndham@gmail.com>
2019-06-07 11:42:35 -0600

    added out class

---
+++ Out.cs
@@ -0,0 +1,33 @@
+using Newtonsoft.Json.Linq;
+using CsvHelper;
+
+namespace Wololo
+{
+    class Out
+    {
+        public static void Csv (string path)
+        {
+
+        }
+ 
+        public static void Http (string url)
+        {
+
+        }
+
+        public static void Json (string path)
+        {
+
+        }
+
+        public static dynamic Object ()
+        {
+
+        }
+
+        public static string Console ()
+        {
+
+        }
+    }
+}
