d561bb84d21543caddce7c332290bf2ad3d3edf8
Wyndham <mattwyndham@byui.edu>
2019-06-13 13:48:43 -0600

    pushing this up to date

--- In.cs
+++ In.cs
@@ -1,35 +1,73 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using CsvHelper;
 using Newtonsoft.Json.Linq;
 
 namespace Wololo
 {
     class In
     {
-        // Convert CSV file to JArray
+
+      
+
+        /// <summary>
+        /// A method in the <c>In</c> class. Takes a CSV file path, 
+        /// opens the file and takes the contents
+        /// and creates a JArray for the user to use
+        /// </summary>
+        /// <param name="path">The path to your CSV File</param>
         internal static JArray jarrFromCSVFile(string path)
         {
-            
+            JArray jarr = new JArray();
+            using (var reader = new StreamReader(path))
+            using (var csv = new CsvReader(reader))
+            {
+                // read records in as a dynamic object
+                var records = csv.GetRecords<dynamic>();
+
+                foreach (dynamic rec in records)
+                {
+                    JObject job = JObject.FromObject(rec);
+                    jarr.Add(job);
+                }
+
+            }
+            return jarr;
         }
-
-        // Convert HTTP url request to JArray
-        internal static JArray jarrFromURL(string url)
-        {
-
-        }
-
 
         // Convert JSON file to JArray 
+        ///<summary>
+        /// A method in the <c>In</c> class. Takes a JSON file path, 
+        /// opens the file and takes the contents
+        /// and creates a JArray for the user to use
+        ///</summary>
+        ///
+        ///<param name="path">The path to your JSON File</param>
         internal static JArray jarrFromJSONFile(string path)
         {
-
+            string json = System.IO.File.ReadAllText(path);
+            JArray jarr = JArray.Parse(json);
+            return jarr;
         }
 
         // Convert C# OBJ to JArray 
-        internal static JArray jarrFromObj(Object obj)
+        /// <summary>
+        /// A method in the <c>In</c> class. Takes an object and
+        /// creates a JArray for the user.
+        /// </summary>
+        ///
+        /// <param name="objects">A list of dynamic type objects</param>
+        internal static JArray jarrFromObj(List<dynamic> objects)
         {
+            // loop through the list of objects and add them to the array.
             JArray jarr = new JArray();
-            return jarr.FromObject(o);
+
+            foreach (dynamic obj in objects)
+            {
+                JObject job = JObject.FromObject(obj);
+                jarr.Add(job);
+            }
+            return jarr;
         }
-     
-        
     }
 }
